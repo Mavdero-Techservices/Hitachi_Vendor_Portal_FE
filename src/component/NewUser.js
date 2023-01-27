d6402f3f6d9eb014e2eb9c3d799a18d50510d04e
vinodhinikaran <vinodhini.thiruvalluvar@gyanmatrix.com>
2023-01-27 11:44:31 +0530

    responsive issue fixed

--- src/component/NewUser.js
+++ src/component/NewUser.js
@@ -1,12 +1,12 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/NewUser.css";
-import template from "../img/newuser.png";
+import template from "../img/userBg.jpg";
 import Logo from "../img/logo.png";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import Image from 'react-bootstrap/Image';
 const NewUser = () => {
   const navigate = useNavigate();
   const onYesButtonClick = useCallback(() => {
@@ -18,23 +18,19 @@ const NewUser = () => {
   }, [navigate]);
 
   return (
-    <div>
-      <Row>
-        <Col>
-          <div className="template">
-            <div className="logo">
-              <img className="hitachi-logo-icon center" alt="" src={Logo} />
-              <div className="container-fluid text-Qa">
-                <h3>Already Registered with HSI?</h3>
-              </div>
-              <div className="redirect-button">
-                <button className="redirectButton Yes" onClick={onYesButtonClick}>yes</button>
-                <button className="redirectButton No" onClick={onNoButtonClick}>No</button>
-              </div>
-            </div>
-          </div>
-        </Col>
-      </Row>
+
+    <div >
+      <Image className="userBackground" src={template} fluid />
+      <div class="content">
+        <img className="hitachi-logo-icon center" alt="" src={Logo} />
+        <h3 className="userTitle">Already Registered with HSI?</h3>
+        <div className="agreebtn">
+          <button type="button" className="btn btn-md m-1 redirectButton Yes" onClick={onYesButtonClick}>yes</button>
+          <br />
+          <button type="button" className="btn btn-md m-1 redirectButton No" onClick={onNoButtonClick}>No</button>
+        </div>
+      </div>
+
     </div>
   );
 };
