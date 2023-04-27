ca86cea443bdeab43b88e6e43aec5bf320ba70af
yellowflash123 <karthigapalani4@gmail.com>
2023-04-27 15:49:57 +0530

    Transparent logo image changed for newuser page

--- src/component/NewUser.js
+++ src/component/NewUser.js
@@ -2,11 +2,12 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/NewUser.css";
 import template from "../img/userBg.jpg";
-import Logo from "../img/logo.png";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import Logo from "../img/logo1.png";
+// import Logo from "../img/logo.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 const NewUser = () => {
   const navigate = useNavigate();
   const onYesButtonClick = useCallback(() => {
@@ -18,19 +19,29 @@ const NewUser = () => {
   }, [navigate]);
 
   return (
-
-    <div >
+    <div>
       <Image className="userBackground" src={template} fluid />
       <div className="content">
         <img className="hitachi-logo-icon center" alt="" src={Logo} />
         <h3 className="userTitle">Already Registered with HSI?</h3>
         <div className="agreebtn">
-          <button type="button" className="btn btn-md m-1 redirectButton Yes" onClick={onYesButtonClick}>yes</button>
+          <button
+            type="button"
+            className="btn btn-md m-1 redirectButton Yes"
+            onClick={onYesButtonClick}
+          >
+            yes
+          </button>
           <br />
-          <button type="button" className="btn btn-md m-1 redirectButton No" onClick={onNoButtonClick}>No</button>
+          <button
+            type="button"
+            className="btn btn-md m-1 redirectButton No"
+            onClick={onNoButtonClick}
+          >
+            No
+          </button>
         </div>
       </div>
-
     </div>
   );
 };
